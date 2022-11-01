9ab63a698b982240a6aff17b87f3ddfc80e3c271
Charlie Xu <byv55f5f5@gmail.com>
2022-11-01 16:48:53 +0800

    Add recursive to travel all the ".".

--- index.js
+++ index.js
@@ -8,23 +8,37 @@ const language = {};
 const { Sheets: sheet, SheetNames: sheetNames } = workbook;
 const json = xlsx.utils.sheet_to_json(sheet[sheetNames[0]]);
 
-json.forEach((string) => {
-  const key = string.KEY;
-  if (key) {
-    Object.keys(string).forEach((langName) => {
-      if (langName !== 'KEY') {
-        if (!language[langName]) language[langName] = {};
-
-        const [key1, key2] = key.split('.');
-        if (key2) {
-          if (!language[langName][key1]) language[langName][key1] = {};
-          language[langName][key1][key2] = string[langName].toString();
-        } else {
-          language[langName][key1] = string[langName].toString();
-        }
+function createNestedObj(languageMap, keys, string) {
+  function recursive(refer, ka) {
+    let copy = [...ka];
+    if (copy.length > 1) {
+      if (!refer[copy[0]]) {
+        refer[copy[0]] = {};
       }
-    });
+      const newRefer = refer[copy[0]];
+      copy = copy.slice(1);
+      return recursive(newRefer, copy);
+    } else {
+      refer[copy[0]] = string.toString();
+    }
   }
+
+  recursive(languageMap, keys);
+}
+
+json.forEach((stringGroup) => {
+  const key = stringGroup.KEY;
+  delete stringGroup.KEY;
+  Object.entries(stringGroup).forEach((entry) => {
+    const [langName, string] = entry;
+    if (!language[langName]) language[langName] = {};
+    const keys = key.split('.');
+    if (keys.length > 1) {
+      createNestedObj(language[langName], keys, string);
+    } else {
+      language[langName][keys[0]] = string.toString();
+    }
+  });
 });
 
 Object.keys(language).forEach((langName) => {
